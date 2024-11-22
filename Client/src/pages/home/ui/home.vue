a0a2a088dbafb6ccf5f60daa9daa409becc8b259
Edanriell <Edanriell@gmail.com>
2024-11-22 12:30:13 +0200

    feature(home-page): created home page

--- Client/src/pages/home/ui/home.vue
+++ Client/src/pages/home/ui/home.vue
@@ -1,11 +1,14 @@
 <script lang="ts" setup>
+	import { AgeCalculator } from "@features/age-calculator/ui";
 	import { MainLayout } from "@widgets/layouts/main/ui";
 </script>
 
 <template>
 	<MainLayout>
-		<h1>Home Page</h1>
-		<section class="home-page"></section>
+		<h1 class="visually-hidden">Home Page</h1>
+		<section class="home-page">
+			<AgeCalculator />
+		</section>
 	</MainLayout>
 </template>
 
