1fbbbb1b0439fa9d5acc471c3b22b9a612af84e4
Edanriell <Edanriell@gmail.com>
2024-11-27 10:58:49 +0200

    feature(error-message-styles): styled error messages

--- Client/src/features/age-calculator/model/validation-schema.ts
+++ Client/src/features/age-calculator/model/validation-schema.ts
@@ -6,15 +6,15 @@ const calculateMinYear = (year: number): number => new Date().getFullYear() - ye
 
 export const birthdateFormValidationSchema = z.object({
 	day: z
-		.number({ invalid_type_error: "Must be a valid day" })
+		.number({ invalid_type_error: "This field is required" })
 		.min(1, "Must be a valid day")
 		.max(31, "Must be a valid day"),
 	month: z
-		.number({ invalid_type_error: "Must be a valid day" })
+		.number({ invalid_type_error: "This field is required" })
 		.min(1, "Must be a valid month")
 		.max(12, "Must be a valid month"),
 	year: z
-		.number({ invalid_type_error: "Must be a valid day" })
+		.number({ invalid_type_error: "This field is required" })
 		.min(calculateMinYear(122), `Year must be no earlier than ${calculateMinYear(122)}`)
 		.max(calculateMaxYear(), "Must be in the past")
 });
